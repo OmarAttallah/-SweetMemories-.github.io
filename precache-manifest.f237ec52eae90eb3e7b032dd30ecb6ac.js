351f9a0ad0a620657dac7eff7e2cb4180ea7ecf8
omarAttallah <omarAttallah3@gmail.com>
2023-03-04 05:25:37 +0200

    Updates

---
+++ precache-manifest.f237ec52eae90eb3e7b032dd30ecb6ac.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec03f0a46d7bed14702347d0ebd521c3",
+    "url": "/SweetMemories/index.html"
+  },
+  {
+    "revision": "e64aa90de0801bc488fa",
+    "url": "/SweetMemories/static/css/main.88f8ddbc.chunk.css"
+  },
+  {
+    "revision": "fb6bdbc6e09ef2a62e7b",
+    "url": "/SweetMemories/static/js/2.5afd48cb.chunk.js"
+  },
+  {
+    "revision": "eca373d6688a5d5efead34244f081e8b",
+    "url": "/SweetMemories/static/js/2.5afd48cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e64aa90de0801bc488fa",
+    "url": "/SweetMemories/static/js/main.4c044eb2.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/SweetMemories/static/js/main.4c044eb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9f18e5dbc70f700e289",
+    "url": "/SweetMemories/static/js/runtime-main.b20f964e.js"
+  },
+  {
+    "revision": "1fefc998281bdc0d6ab0e4dfb9bb9d0f",
+    "url": "/SweetMemories/static/media/memories.1fefc998.png"
+  }
+]);
